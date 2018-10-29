0101376ee5e44f590e4733b5abbfea4be550f6bf
GUI-NOTEBOOK\Guilherme Abacherli <guilherme.abacherli@fatec.sp.gov.br>
2018-10-28 23:43:57 -0300

    Adicionadas as classes e layouts referentes as telas que foram definidas no wireframe do projeto.

---
+++ app/src/main/java/br/com/pi5s/icomida/MesasActivity.java
@@ -0,0 +1,13 @@
+package br.com.pi5s.icomida;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MesasActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mesas);
+    }
+}
